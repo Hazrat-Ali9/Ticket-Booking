0edf477d950cd34859c909aa36fa7c4b719a66da
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-31 10:29:20 +0600

    payment cencel  🚀

--- src/pages/Dashboard/Payment/PaymentCencel.jsx
+++ src/pages/Dashboard/Payment/PaymentCencel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { AiOutlineCheckCircle } from "react-icons/ai";
-
+// payment cencel
 const PaymentCencel = () => {
   const navigate = useNavigate();
   return (
